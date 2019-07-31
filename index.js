497880a818e68c9d2e512dd61cf2c68f42c8db0f
Kay Jay <kharris2k16@gmail.com>
2019-07-31 16:58:50 -0700

    Progress on mvp

--- index.js
+++ index.js
@@ -13,32 +13,4 @@ server.get('/', (req, res) => {
 
 server.use('/posts', postRoutes);
 
-// server.post('/api/posts', (req, res) => {
-//   const { title, contents } = req.body;
-
-//   if(!title || !contents){
-//     res
-//     .status(400).json({errorMessage: 'Post must contain a title and contents'})
-//   }
-//   else {
-//     db.insert(req.body)
-//       .then(db => {
-//         res.status(201).json(db);
-//       })
-//       .catch(() => {
-//         res.status(500).json({
-//           errorMessage:'There was an error while saving the post to the database',
-//         })
-//       })
-//   }
-// })
-
-// //get all posts
-// server.get('/api/posts', (req, res) => {
-//   db
-//     .find()
-//     .then(posts => res.status(200).json(posts))
-//     .catch(err => res.status(500).json({error: 'The posts information could not be retrieved'}));
-// });
-
 server.listen(8250, () => console.log('API running on port 8250'));
--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -2,14 +2,29 @@ const express = require('express')
 const router = express.Router()
 const db = require('../data/db')
 
-//get all posts
-router.get('/', (req, res) => {
-  db
-    .find()
-    .then(posts => res.status(200).json(posts))
-    .catch(err => res.status(500).json({error: 'The posts information could not be retrieved'}));
-});
-
+//create a comment for a post with a specified id
+router.post('/:id/comments', (req, res) => {
+  const commentInfo = { ...req.body, post_id: req.params.id };
+  console.log(commentInfo);
+  if(!req.body.text){
+    res
+    .status(400)
+    .json({ errorMessage: 'Comment missing required field'})
+  }
+  else{
+    db.insertComment(commentInfo)
+    .then(db => {
+      res.status(201).json(db);
+    })
+    .catch(() => {
+      res.status(500).json({
+        errorMessage:
+          'There was an error while saving the post to the database',
+      })
+    })
+  }
+})
+//create new post
 router.post('/', (req, res) => {
   const { title, contents } = req.body;
 
@@ -29,14 +44,74 @@ router.post('/', (req, res) => {
       })
   }
 })
-
-
+//get all posts
+router.get('/', (req, res) => {
+  db
+    .find()
+    .then(posts => res.status(200).json(posts))
+    .catch(err => res.status(500).json({error: 'The posts information could not be retrieved'}));
+});
+//get post by ID
 router.get('/:id', (req, res) => {
-  res.status(200).send('hello from the GET /posts/:id endpoint');
+  const { id } = req.params;
+  db
+    .findById(id)
+    .then(posts => res.status(200).json(posts))
+    .catch(err => res.status(500).json({error: `The posts information could not be retrieved ${err}`}));
+});
+// get all comments -- Working
+router.get('/:id/comments', (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  db
+  .findPostComments(id)
+  .then(comments =>
+    comments
+    ? res.status(200).json(comments)
+    : res.status(500).json({error: `The comment with the specified ID does not exist`})
+    .catch(err => res.status(500)
+    .json({error: `The user information could not be retrieved ${err}`})))
 });
 
-router.post('/', (req, res) => {
-  res.status(200).send('hello from the POST /posts endpoint');
-});
+
+// router.get('/:id/comments', (req, res) => {
+//   const { id } = req.params;
+//   db
+//    .findById(id)
+//    .then(post => {
+//     if (post.length > 0) {
+//      db
+//       .findPostComments(id)
+//       .then(comment => {
+//        res.status(200).json(comment);
+//       })
+//       .catch(err => {
+//        res
+//         .status(500)
+//         .json({ error: 'The comments information could not be retrieved.' });
+//       });
+//     } else {
+//      res
+//       .status(404)
+//       .json({ message: 'The post with the specified ID does not exist' });
+//     }
+//    })
+//    .catch(err => {
+//     res
+//      .status(500)
+//      .json({ error: 'The comments information could not be retrieved.' });
+//    });
+//   });
+
+
+
+
+// router.get('/:id', (req, res) => {
+//   res.status(200).send('hello from the GET /posts/:id endpoint');
+// });
+
+// router.post('/', (req, res) => {
+//   res.status(200).send('hello from the POST /posts endpoint');
+// });
 
 module.exports = router;
